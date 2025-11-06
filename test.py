a3471184ebfc7c10a80be2ac67ad84e010092f61
flowerswine <2914518090@qq.com>
2025-11-06 12:23:05 +0800

    chore: 添加 langchain deepagent 测试

--- test.py
+++ test.py
@@ -1,25 +1,44 @@
-from dataclasses import dataclass
-from langchain.agents import create_agent
-from langchain.agents.middleware import dynamic_prompt, ModelRequest
+import os
+from typing import Literal
+from tavily import TavilyClient
+from deepagents import create_deep_agent
+from dotenv import load_dotenv
 
+load_dotenv()
 
-@dataclass
-class ContextSchema:
-    user_name: str
+tavily_client = TavilyClient(api_key=os.environ["TAVILY_API_KEY"])
 
-@dynamic_prompt
-def personalized_prompt(request: ModelRequest) -> str:  
-    user_name = request.runtime.context.user_name
-    return f"You are a helpful assistant. Address the user as {user_name}."
+def internet_search(
+    query: str,
+    max_results: int = 5,
+    topic: Literal["general", "news", "finance"] = "general",
+    include_raw_content: bool = False,
+):
+    """Run a web search"""
+    return tavily_client.search(
+        query,
+        max_results=max_results,
+        include_raw_content=include_raw_content,
+        topic=topic,
+    )
 
-agent = create_agent(
-    model="claude-sonnet-4-5-20250929",
-    tools=[get_weather],
-    middleware=[personalized_prompt],
-    context_schema=ContextSchema
+# System prompt to steer the agent to be an expert researcher
+research_instructions = """You are an expert researcher. Your job is to conduct thorough research and then write a polished report.
+
+You have access to an internet search tool as your primary means of gathering information.
+
+## `internet_search`
+
+Use this to run an internet search for a given query. You can specify the max number of results to return, the topic, and whether raw content should be included.
+"""
+
+agent = create_deep_agent(
+    model="deepseek-chat",
+    tools=[internet_search],
+    system_prompt=research_instructions
 )
 
-agent.invoke(
-    {"messages": [{"role": "user", "content": "what is the weather in sf"}]},
-    context=ContextSchema(user_name="John Smith")  
-)
+result = agent.invoke({"messages": [{"role": "user", "content": "AE 的AI插件有哪些？"}]})
+
+# Print the agent's response
+print(result["messages"][-1].content)
